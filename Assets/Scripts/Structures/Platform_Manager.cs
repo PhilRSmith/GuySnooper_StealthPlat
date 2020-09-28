d2492799526914d29f23cdefc9a1ff3c4c824911
PhilRSmith <theonepudgypanda@gmail.com>
2020-09-28 11:44:50 -0400

    chngd platform mngr: allows degree input direction

--- Assets/Scripts/Structures/Platform_Manager.cs
+++ Assets/Scripts/Structures/Platform_Manager.cs
@@ -2,13 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+*Simple platform manager that can be attached to a platform object and used in unity editor to designate a specific direction using degrees to move in
+*Allows a speed for the platform to also be specified in the unity editor
+*TODO:Accounts for the playercontroller being on the platform and makes it move with itself smoothly.
+*/
 public class Platform_Manager : MonoBehaviour
 {
     private GameObject _player;
+    private bool _playerOnPlatform = false;
+    private Vector3 _playerMovementOffset = new Vector3(0,0,0);
     [SerializeField]
     private GameObject _parentObject;
     [SerializeField]
-    private Vector3 _platDirection;
+    private float _platDirectionDegrees=0; //This is a float value that is converted to radians and then cartesian coordinates for movement.
+    private Vector3 _platBaseCartesianDirection;
+    private Vector3 _currentCartesianDirection;
+    [SerializeField]
     private float _platSpeed = 1.0f;
     
     [SerializeField]
@@ -26,60 +36,64 @@ public class Platform_Manager : MonoBehaviour
         {
             Debug.LogError("Platform:ParentPlatform NOT FOUND");
         }
+
+        /*Setting the degree direction specified to cartesian coordinates for platform movement.*/
+        float directionInRadians = _platDirectionDegrees * ( Mathf.PI / ((float)180.0));
+        float cartX_direction = Mathf.Cos(directionInRadians);
+        float cartY_direction = Mathf.Sin(directionInRadians);
+        _platBaseCartesianDirection.x = cartX_direction; _platBaseCartesianDirection.y = cartY_direction; _platBaseCartesianDirection.z = 0;
+
+        _currentCartesianDirection = _platBaseCartesianDirection;   
+        _alternationSwitchTime = Time.time + _alternationRate; //set first direction switch
     }
 
     // Update is called once per frame
     void Update()
     {
-
-       
+        AlternateDirections();
+       _parentObject.transform.Translate(_currentCartesianDirection * _platSpeed * Time.deltaTime);
     }
 
-
-    private void AlternateDirections(string stringForDirection)
+    void LateUpdate() 
     {
-        if(stringForDirection.Equals("upAndDown", System.StringComparison.OrdinalIgnoreCase))
-        {
-            AlternateUpAndDown();
-        }
-
-        if(stringForDirection.Equals("rightAndLeft", System.StringComparison.OrdinalIgnoreCase))
-        {
-            AlternateUpAndDown();
-        }
+        //movePlayerIfOnPlatform();
     }
-    private void AlternateUpAndDown()
+
+    private void AlternateDirections()
     {
         if(Time.time>_alternationSwitchTime)
         {
-           
+            _alternationSwitchTime = Time.time + _alternationRate;
+            _currentCartesianDirection *= -1;
         }
     }
 
-    private void AlternateRightAndLeft()
+    private void movePlayerIfOnPlatform()
     {
-        if(Time.time>_alternationSwitchTime)
+        if(_playerOnPlatform)
         {
-           
+            _player.transform.position = _parentObject.transform.position+_playerMovementOffset;
         }
     }
-    
-    private void OnTriggerEnter(Collider other) 
+
+    private void OnTriggerStay(Collider other) 
     {
         if(other.gameObject.tag=="Player")
         {
             //Debug.Log("Player on Platform");
-            _player.transform.parent = _parentObject.transform;
+            _playerMovementOffset = _player.transform.position - _parentObject.transform.position;
+            _playerOnPlatform = true;
             _player.GetComponent<PlayerController>().SetVerticalWhileOnPlatform(_platSpeed);
         }
     }
+
 
     private void OnTriggerExit(Collider other) 
     {
         if(other.gameObject.tag=="Player")
         {
-            //Debug.Log("Player off Platform");
-            _player.transform.parent = null;
+            //Debug.Log("Player off Platform")
+            _playerOnPlatform = false;
             _player.GetComponent<PlayerController>().ResetVertical();
         }
     }
